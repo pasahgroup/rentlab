6491878b4825e827aa44169b197529d708d4118b
pasahgroup <info@pasah.net>
2024-08-19 11:30:13 +0300

    19 Aug 2024

--- app/Models/cartype.php
+++ app/Models/cartype.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Cartype extends Model
+class cartype extends Model
 {
     use HasFactory;
         protected $fillable  = [
