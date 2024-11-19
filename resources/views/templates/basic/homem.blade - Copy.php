ff47b43df4d45d6dd25dd6b39854fb216bab18a7
pasahgroup <info@pasah.net>
2024-11-19 16:46:10 +0300

    19 11 2024

--- resources/views/templates/basic/homem.blade - Copy.php
+++ resources/views/templates/basic/homem.blade - Copy.php
@@ -6,13 +6,13 @@
             <div id="carouselId" class="carousel slide" data-bs-ride="carousel" data-bs-interval="false">
                 <ol class="carousel-indicators">
                     <li data-bs-target="#carouselId" data-bs-slide-to="0" class="active" aria-current="true" aria-label="First slide"></li>
-                    <li data-bs-target="#carouselId" data-bs-slide-to="1" aria-label="Second slide"></li>
-                    <li data-bs-target="#carouselId" data-bs-slide-to="2" aria-label="Second slide"></li>
-                    
+                       @foreach ($vehicles as $indexKey => $veh)
+                    <li data-bs-target="#carouselId" data-bs-slide-to="{{$indexKey+1}}" aria-label="Second slide"></li>
+                     @endforeach
                 </ol>
                 <div class="carousel-inner" role="listbox">
-                    
-                    <div class="carousel-item active">              
+                    <div class="carousel-item active">
+                        
                         <img src="../../frontendp/img/carousel-2.jpg" class="img-fluid w-100" alt="First slide"/>
                         <div class="carousel-caption">
                             <div class="container py-4">
@@ -82,81 +82,13 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>                         
+                        </div>
+                         
                     </div>
 
- <div class="carousel-item">
-                        <img src="#" alt="rent-vehicle" class="img-fluid w-100" alt="First slide"/>
-                        <div class="carousel-caption">
-                            <div class="container py-4">
-                                <div class="row g-5">
-                                    <div class="col-lg-6 fadeInLeft animated" data-animation="fadeInLeft" data-delay="1s" style="animation-delay: 1s;">
-                                        <div class="bg-secondary rounded p-5">
-                                            <h4 class="text-white mb-4">CONTINUE CAR RESERVATION</h4>
-                                            <form>
-                                                <div class="row g-3">
-                                                    <div class="col-12">
-                                                        <select class="form-select" aria-label="Default select example">
-                                                            <option selected>Select Your Car type</option>
-                                                            <option value="1">VW Golf VII</option>
-                                                            <option value="2">Audi A1 S-Line</option>
-                                                            <option value="3">Toyota Camry</option>
-                                                            <option value="4">BMW 320 ModernLine</option>
-                                                        </select>
-                                                    </div>
-                                                    <div class="col-12">
-                                    <div class="input-group">
-                            <div class="d-flex align-items-center bg-light text-body rounded-start p-2">
-                            <span class="fas fa-map-marker-alt"></span><span class="ms-1">Pick Up</span>
-                                                            </div>
-                        <input class="form-control" type="text" placeholder="Enter a City or Airport" aria-label="Enter a City or Airport">
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-12">
-    <a href="#" class="text-start text-white d-block mb-2">Need a different drop-off location?</a>
-<div class="input-group">
-<div class="d-flex align-items-center bg-light text-body rounded-start p-2">
-<span class="fas fa-map-marker-alt"></span><span class="ms-1">Drop off</span>
-                                                            </div>
-<input class="form-control" type="text" placeholder="Enter a City or Airport" aria-label="Enter a City or Airport">
-                                                        </div>
-                                                    </div>
-                                    <div class="col-12">
-                                        <div class="input-group">
-<div class="d-flex align-items-center bg-light text-body rounded-start p-2">
-        <span class="fas fa-calendar-alt"></span><span class="ms-1">Pick Up</span>
-                                                            </div>
-<input class="form-control" type="date">
-                                                        </div>
-                                                    </div>
-                                            <div class="col-12">
-                                    <div class="input-group">
-                                <div class="d-flex align-items-center bg-light text-body rounded-start p-2">
-                                <span class="fas fa-calendar-alt"></span><span class="ms-1">Drop off</span>
-                                                            </div>
-                        <input class="form-control" type="date">
-                    
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-12">
-                                                        <button class="btn btn-light w-100 py-2">Book Now</button>
-                                                    </div>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6 d-none d-lg-flex fadeInRight animated" data-animation="fadeInRight" data-delay="1s" style="animation-delay: 1s;">
-                                        <div class="text-start">
-                                            <strong class="text-white"></strong>
-                                            <p></p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 
                     <div class="carousel-item">
+                           @foreach ($vehicles as $index => $vehicle)
                         <img src="#" alt="rent-vehicle" class="img-fluid w-100" alt="First slide"/>
                         <div class="carousel-caption">
                             <div class="container py-4">
@@ -218,13 +150,14 @@
                                     </div>
                                     <div class="col-lg-6 d-none d-lg-flex fadeInRight animated" data-animation="fadeInRight" data-delay="1s" style="animation-delay: 1s;">
                                         <div class="text-start">
-                                            <strong class="text-white">kl</strong>
-                                            <p>kk</p>
+                                            <strong class="text-white">{{$vehicle->model}}</strong>
+                                            <p>{{$vehicle->details}}</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                                @endforeach
                     </div>
                 </div>
             </div>
