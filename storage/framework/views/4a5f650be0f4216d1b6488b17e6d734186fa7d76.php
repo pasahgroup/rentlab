72bf81a2aa28768ab6c217a41f8b959f36298054
pasahgroup <info@pasah.net>
2024-11-20 13:57:48 +0300

    20 11 2024

--- storage/framework/views/4a5f650be0f4216d1b6488b17e6d734186fa7d76.php
+++ storage/framework/views/4a5f650be0f4216d1b6488b17e6d734186fa7d76.php
@@ -265,7 +265,7 @@
 
  
  <!-- Car categories Start -->
-        <div class="container-fluid categories pb-5" id="section3">
+        <div class="container categories pb-5" id="section3">
                 <div class="text-center mx-auto pb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 800px;">
                     <h1 class="display-5 text-capitalize mb-3">Vehicle <span class="text-primary">Categories</span></h1>
                     <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut amet nemo expedita asperiores commodi accusantium at cum harum, excepturi, quia tempora cupiditate! Adipisci facilis modi quisquam quia distinctio,
@@ -317,85 +317,6 @@
                     </div>
 
         </div>
-
-
-        <!-- About Start -->
-        <div class="container-fluid overflow-hidden about py-5" id="section5">
-            <div class="container py-5">
-                <div class="row g-5">
-                    <div class="col-xl-6 wow fadeInLeft" data-wow-delay="0.2s">
-                        <div class="about-item">
-                            <div class="pb-5">
-                                <h1 class="display-5 text-capitalize">Cental <span class="text-primary">About</span></h1>
-                                <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut amet nemo expedita asperiores commodi accusantium at cum harum, excepturi, quia tempora cupiditate! Adipisci facilis modi quisquam quia distinctio,
-                                </p>
-                            </div>
-                            <div class="row g-4">
-                                <div class="col-lg-6">
-                                    <div class="about-item-inner border p-4">
-                                        <div class="about-icon mb-4">
-                                            <img src="../../frontendp/img/about-icon-1.png" class="img-fluid w-50 h-50" alt="Icon">
-                                        </div>
-                                        <h5 class="mb-3">Our Vision</h5>
-                                        <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6">
-                                    <div class="about-item-inner border p-4">
-                                        <div class="about-icon mb-4">
-                                            <img src="../../frontendp/img/about-icon-2.png" class="img-fluid h-50 w-50" alt="Icon">
-                                        </div>
-                                        <h5 class="mb-3">Our Mision</h5>
-                                        <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <p class="text-item my-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, aliquam ipsum. Sed suscipit dolorem libero sequi aut natus debitis reprehenderit facilis quaerat similique, est at in eum. Quo, obcaecati in!
-                            </p>
-                            <div class="row g-4">
-                                <div class="col-lg-6">
-                                    <div class="text-center rounded bg-secondary p-4">
-                                        <h1 class="display-6 text-white">17</h1>
-                                        <h5 class="text-light mb-0">Years Of Experience</h5>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6">
-                                    <div class="rounded">
-                                        <p class="mb-2"><i class="fa fa-check-circle text-primary me-1"></i> Morbi tristique senectus</p>
-                                        <p class="mb-2"><i class="fa fa-check-circle text-primary me-1"></i> A scelerisque purus</p>
-                                        <p class="mb-2"><i class="fa fa-check-circle text-primary me-1"></i> Dictumst vestibulum</p>
-                                        <p class="mb-0"><i class="fa fa-check-circle text-primary me-1"></i> dio aenean sed adipiscing</p>
-                                    </div>
-                                </div>
-                                <div class="col-lg-5 d-flex align-items-center">
-                                    <a href="#" class="btn btn-primary rounded py-3 px-5">More About Us</a>
-                                </div>
-                                <div class="col-lg-7">
-                                    <div class="d-flex align-items-center">
-                                        <img src="../../frontendp/img/attachment-img.jpg" class="img-fluid rounded-circle border border-4 border-secondary" style="width: 100px; height: 100px;" alt="Image">
-                                        <div class="ms-4">
-                                            <h4>William Burgess</h4>
-                                            <p class="mb-0">Carveo Founder</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 wow fadeInRight" data-wow-delay="0.2s">
-                        <div class="about-img">
-                            <div class="img-1">
-                                <img src="../../frontendp/img/about-img.jpg" class="img-fluid rounded h-100 w-100" alt="">
-                            </div>
-                            <div class="img-2">
-                                <img src="../../frontendp/img/about-img-1.jpg" class="img-fluid rounded w-100" alt="">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- About End -->
 
         <!-- Fact Counter -->
         <div class="container-fluid counter bg-secondary py-5" id="section6">
